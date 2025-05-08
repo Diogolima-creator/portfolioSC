03d568284d010ec7248bfab5ab58c2dbc4273626
diogolimadev <diogolima@evop.com.br>
2025-05-07 22:56:54 -0500

    fix

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useContext, useState } from 'react'
+import type { ReactNode } from 'react'
+import { createContext, useContext, useState } from 'react'
 
 type Language = 'pt' | 'en'
 
